3e76767a8f605c9e574b7a9867bfe10132e83477
Dmytro Boiarchuk <dimabwhite98@gmail.com>
2023-02-19 17:20:29 +0000

    Changes in the main branch

---
+++ src/store/modules/slotHeihgt/actions.ts
@@ -0,0 +1,17 @@
+import store from '../../store';
+
+export const putHeight = (height: number) => {
+  store.dispatch({ type: 'CURRENT_HEIGHT', payload: height });
+};
+export const setPrevId = (id: number) => {
+  store.dispatch({ type: 'PREV_ID', payload: id });
+};
+export const deletePrevId = () => {
+  store.dispatch({ type: 'DELETE_ID' });
+};
+export const setCurrentCard = (id: number) => {
+  store.dispatch({ type: 'SET_CURRENT_CARD', payload: id });
+};
+export const setLastSlot = (id: number) => {
+  store.dispatch({ type: 'SET_LAST_SLOT', payload: id });
+};
---
+++ src/store/modules/slotHeihgt/reducer.ts
@@ -0,0 +1,39 @@
+const initialState = {
+  slotHeight: 0,
+  prevId: 0,
+  currentCard: 0,
+  last: 0,
+};
+
+export default function reducer(state = initialState, action: { type: string; payload?: any }) {
+  switch (action.type) {
+    case 'CURRENT_HEIGHT':
+      return {
+        ...state,
+        slotHeight: action.payload,
+      };
+    case 'PREV_ID':
+      return {
+        ...state,
+        prevId: action.payload,
+      };
+    case 'SET_CURRENT_CARD':
+      return {
+        ...state,
+        currentCard: action.payload,
+      };
+    case 'DELETE_ID':
+      return {
+        ...state,
+        prevId: 0,
+      };
+    case 'SET_LAST_SLOT':
+      return {
+        ...state,
+        last: action.payload,
+      };
+    default: {
+      return { ...state };
+    }
+  }
+}
